f86248f5ccfe3f7adb8f401e14bb17a82a18ac89
Jane <spring1981@mail.ru>
2022-08-08 21:37:26 +0300

    task41 updated

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateNewArray(int a)
+{
+   int[] array = new int[a];
+
+    for (int i = 0; i < array.Length; i++) 
+    {
+        Console.WriteLine("Input array element ");
+        array [i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int MoreThanZero(int[] array)
+{
+    int quantity = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            quantity++;
+    }
+    return quantity;
+}
+
+Console.Write("Input amount of elements: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateNewArray(a);
+Console.WriteLine("Amount of elements is: " + MoreThanZero(array));
